4768070851b1eb8ee309dcaade07e0865986caac
Jamone Kelly <xg.monie089@gmail.com>
2015-05-07 19:56:15 -0400

    Initial Commit

---
+++ Jaria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jaria
+//
+//  Created by Jamone Alexander Kelly on 5/7/15.
+//  Copyright (c) 2015 Jaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
